4233e50185b3a7f3b7f9ff5497c84480daf14ad0
buivan2002 <buiquocanh1612@gmail.com>
2025-03-05 18:50:19 +0700

    boostrap test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :charts
+  get 'pages/custom'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :charts 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/custom_page", to: "pages#custom"
+
+  
 end
