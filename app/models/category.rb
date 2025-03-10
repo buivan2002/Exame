5fc03955dac3488aa66ead32f1a1fb139cdc1f97
buinguyenbinh <binhbn@lotusys.vn>
2025-03-10 10:40:50 +0700

    fix association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,8 @@
 class Category < ApplicationRecord
-  has_many :charts
-  has_many :favorites
-  has_many :point_rules
-  has_many :questions
-  has_many :quiz_results
-  has_many :quizzes
-  has_many :sort_orders
-  has_many :statistics
-  has_many :unlocked_levels
-  has_many :user_answers
+  has_many :questions, dependent: :destroy
+  has_many :quizzes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :statistics, dependent: :destroy
+  has_many :point_rules, dependent: :destroy
+  has_many :unlocked_levels, dependent: :destroy
 end
