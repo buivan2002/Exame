d096bf5ec20ced9b1949cf38297076b2838d7e25
dovv@lotusys.com.vn <dovv@lotusys.com.vn>
2025-03-20 18:41:00 +0700

    fix cocoo

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,12 +1,3 @@
-import "controllers";
-import "./controllers";
-import "@rails/ujs";
-import "@nathanvda/cocoon";
-Rails.start();
-
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
-
-//= require jquery
-//= require cocoon
+import "@hotwired/turbo-rails"
+import "controllers"
+import "@nathanvda/cocoon"
